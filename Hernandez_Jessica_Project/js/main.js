c219f106821d6face51e4abdefeca95a1b3fc037
Jessica Hernandez <jesherart@fullsail.edu>
2015-03-11 22:48:19 -0400

    week 2 project updates

--- Hernandez_Jessica_Project/js/main.js
+++ Hernandez_Jessica_Project/js/main.js
@@ -9,20 +9,21 @@ Full Sail University
 
 (function ($) {
 	
-//    Add to bookmarks functionality for the bookmark banner on the homepage
-//    $("#bookmarkme").click(function() {
-//            // Mozilla Firefox Bookmark
-//            if ('sidebar' in window && 'addPanel' in window.sidebar) { 
-//                window.sidebar.addPanel(location.href,document.title,"");
-//            } else if( /*@cc_on!@*/false) { // IE Favorite
-//                window.external.AddFavorite(location.href,document.title); 
-//            } else { // webkit - safari/chrome
-//                alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != - 1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
-//            }
-//        });
-//	
+    // Add to bookmarks functionality for the bookmark banner on the homepage
+    $("#bookmarkme").click(function () {
+        // Mozilla Firefox Bookmark
+        if ('sidebar' in window && 'addPanel' in window.sidebar) { 
+            window.sidebar.addPanel(location.href, document.title, "");
+        } else if (/*@cc_on!@*/false) { // IE Favorite
+            window.external.AddFavorite(location.href, document.title); 
+        } else { // webkit - safari/chrome
+            alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') !== -1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
+        }
+    });
+	
     
     
+    // Tooltip animation with added on styling.
     $('.masterTooltip').hover(function () {
         
         var title = $(this).attr('title');
@@ -41,9 +42,26 @@ Full Sail University
         $('.tooltip').css({top: mouseY, left: mouseX});
         
     });
-	
-		
-
+    
+    
+    
+    //  Tabbed navigation interactivity
+    $('#tabs div[id]').hide().eq(0).show();
+    $('#tabs div[id]:not(:first)').hide();
+    
+    $('#tabs-nav li').click(function (e) { 
+        e.preventDefault();
+        $('#tabs div[id]').hide();
+        
+        $('#tabs-nav .current').removeClass('current');
+        $(this).addClass('current');
+        var clicked = $(this).find('a:first').attr('href');
+        
+        $('#tabs ' + clicked).fadeIn('fast');
+    }).eq(0).addClass('current');
+    
+    
+                         
 	
 })(jQuery); // end private scope
 
